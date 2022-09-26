0018b4dec31329b94847353e973b0a0f317b5d94
bakht200 <62112170+bakht200@users.noreply.github.com>
2022-09-26 23:41:00 +0500

    cometchat

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:intl/intl.dart';
+
+class CometChatAuthConstants {
+  static const String appId = "2193809961454a39";
+  static const String authKey = "b1e544f860ded6a0d1f1ad3d3ede3b048cb7cf1a";
+  static const String region = "us";
+  static const String apiKey = "e324e7af067a5a7164e16c909ad184a631c0984f";
+}
+
+String USERID = "";
+final DateFormat receiptFormatter = DateFormat('hh:mm a');
